42521a98dba9686a5dcc70c31dc2049555f54aeb
JoanCurrie <jecurrie8@gmail.com>
2015-04-16 19:31:55 -0400

    First set of tests

--- calculator_test.rb
+++ calculator_test.rb
@@ -2,5 +2,17 @@ gem "minitest"
 require "minitest/autorun"
 require_relative "calculator"
 
-class TestCalculator < MiniTest::Test
+class TestCalculator < MiniTest::Unit::TestCase
+  def test_string_of_1_return_1
+    assert_equal 1, Calculator.add("1")
+  end
+  
+  def test_a_string_return_number
+    number = rand(100)
+    assert_equal number, Calculator.add(number.to_s)
+  end
+  
+  def test_empty_string_returns_0
+    assert_equal 0, Calculator.add("")
+  end  
 end
